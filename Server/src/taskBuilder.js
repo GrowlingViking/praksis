d603c075ce6c383b5e6a49fa148439bf7b1a09e4
GrowlingViking <siggesg@hotmail.com>
2017-08-24 16:01:15 +0200

    Added init, add, and delete methods to taskBuilder

---
+++ Server/src/taskBuilder.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+/* Create the task table in the database */
+function init() {
+	db.run('CREATE TABLE tasks (id INTEGER, name TEXT, done INTEGER)')
+};
+
+/* Add a task to the database */
+function add(task) {
+	db.run('INSERT INTO tasks (id, name, done) VALUES (' + task.getId() + ', ' + task.getName() + ', ' + task.getDone() + ')')
+};
+
+/* Delete a task from the database */
+function delete(task) {
+	db.run('DELETE FROM tasks WHERE id = ' + task.getId())
+};
